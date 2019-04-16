64664d72d8ee6f122046a82a6cf9a23b5ce97fc5
Fred Rajaona <fred@quitoque.fr>
2019-04-16 13:36:10 +0200

    Implements xcodeproj

---
+++ YouTubeiOSPlayerHelper/YouTubeiOSPlayerHelper.h
@@ -0,0 +1,19 @@
+//
+//  YouTubeiOSPlayerHelper.h
+//  YouTubeiOSPlayerHelper
+//
+//  Created by Fred Rajaona on 16/04/2019.
+//  Copyright © 2019 Quitoque. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YouTubeiOSPlayerHelper.
+FOUNDATION_EXPORT double YouTubeiOSPlayerHelperVersionNumber;
+
+//! Project version string for YouTubeiOSPlayerHelper.
+FOUNDATION_EXPORT const unsigned char YouTubeiOSPlayerHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YouTubeiOSPlayerHelper/PublicHeader.h>
+
+
